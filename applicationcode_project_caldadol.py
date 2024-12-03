325f14742db7dc9d75eaa1787208148b8eba5c1f
lcaldado <luciacaldadorein@gmail.com>
2024-12-03 17:36:36 -0500

    Add files via upload

---
+++ applicationcode_project_caldadol.py
@@ -0,0 +1,308 @@
+import pymysql
+
+#Function to create an account 
+def create_account(connection): 
+    successful_creation=False
+    cursor1=connection.cursor()
+    while not successful_creation:
+        try:
+            mail= input('Introduce mail: ')
+            while mail==None:
+                mail= input('Introduce valid mail: ')
+            first_name=input('Introduce first_name : ')
+            last_name=input('Introduce last_name : ')
+            age=input('Introduce age: ')
+            gender=input('Introduce gender: ')
+            height=input('Introduce height: ')
+            weight=input('Introduce weight: ')
+            cursor1.callproc('create_user', args=[mail,first_name,last_name,age,gender,height,weight])
+            connection.commit()
+            cursor1.close()
+            successful_creation=True
+        except pymysql.MySQLError as error:
+            print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+            print("Please try again.")
+    #Now we find the user_id created 
+    sql_statement='SELECT user_id FROM user ORDER BY user_id DESC LIMIT 1'
+    cursor2=connection.cursor()
+    cursor2.execute(sql_statement)
+    user_id=cursor2.fetchone()
+    cursor2.close()
+    return user_id
+
+#Function to add a new goal
+def add_new_goal(connection,user_id):
+    successful_operation=False
+    cursor1=connection.cursor()
+    while not successful_operation:
+        try:
+            type=input('Introduce type of goal : ')
+            target_value=input('Introduce target value: ')
+            start_date=input('Introduce start date : ')
+            target_date=input('Introduce target date: ')
+            status=input('Introduce status of goal (in progress / finished / not started) : ')
+            cursor1.callproc('add_goal', args=[user_id,type,target_value,start_date,target_date,status])
+            connection.commit()
+            cursor1.close()
+            successful_operation=True
+        except pymysql.MySQLError as error:
+            print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+            print("Please try again.")  
+    print('Goal added successfully!')
+
+#Function to track health test
+def track_health_test(connection,user_id):
+    successful_operation=False
+    cursor1=connection.cursor()
+    while not successful_operation:
+        try:
+            body_fat=input('Introduce body fat % : ')
+            heart_rate=input('Introduce heart rate: ')
+            blood_pressure=input('Introduce blood pressure: ')
+            date=input('Introduce date: ')
+            cursor1.callproc('track_health_test', args=[user_id,body_fat,heart_rate,blood_pressure,date])
+            connection.commit()
+            cursor1.close()
+            successful_operation=True
+        except pymysql.MySQLError as error:
+            print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+            print("Please try again.")
+    print('Health test added successfully!')
+
+#Function to track a meal
+def track_meal(connection,user_id): 
+    successful_operation=False
+    cursor1=connection.cursor()
+    while not successful_operation:
+        try:
+            goal_id=input('Introduce goal id that it is related to this meal: ')
+            meal_date=input('Introduce date : ')
+            type=input('Introduce type of meal: ')
+            calories=input('Introduce number of calories: ')
+            protein=input('Introduce grams of protein: ')
+            carbs=input('Introduce grams of carbs: ')
+            fat=input('Introduce grams of fat: ')
+            cursor1.callproc('track_meal', args=[goal_id,user_id,meal_date,type,calories,protein,carbs,fat])
+            connection.commit()
+            cursor1.close()
+            successful_operation=True
+        except pymysql.MySQLError as error:
+            print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+            print("Please try again.")
+    print('Meal added succesfully!')
+
+#Function to add exercise to a workout
+def add_exercise_to_workout(connection,workout_id):
+    print('1. Add exercise already created')
+    print('2. Add new exercise')
+    choice=input('Choose an option (1 or 2) : ')
+    if choice=='1':
+        while True: 
+            cursor1=connection.cursor()
+            sql_stam="select * from exercise"
+            cursor1.execute(sql_stam)
+            possible_exercises=cursor1.fetchall()
+            possible_exercise_names=[ex['exercise_name'] for ex in possible_exercises]
+            print('List of exercise names : ')
+            for ex in possible_exercise_names:
+                print(ex)
+            cursor1.close()
+            exercise_name=input('Enter exercise name that you want to add to workout: ')
+            if exercise_name in possible_exercise_names:
+                break
+            else:
+                print('exercise name not valid')
+        successful_operation=False
+        cursor2=connection.cursor()
+        while not successful_operation:
+            try:
+                cursor2.callproc('new_exercise_in_workout', args=[exercise_name,workout_id])
+                connection.commit()
+                cursor2.close()
+                successful_operation=True
+            except pymysql.MySQLError as error:
+                print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+                print("Please try again.")
+        print('Exercise added to workout!')
+    elif choice=='2':
+        successful_operation=False
+        cursor3=connection.cursor()
+        while not successful_operation:
+            try:
+                exercise_name=input('Introduce name of the new exercise: ')
+                type=input('Introduce type of exercise (cardio or strength): ')
+                if type=='cardio':
+                    distance=input('Introduce distance(km): ')
+                    velocity=input('Introduce velocity: ')
+                    resistance=input('Introduce resistance: ')
+                    machine= None
+                    material= None
+                    reps= None
+                    sets=None
+                    weight=None
+                elif type=='strength':
+                    distance=None
+                    velocity=None
+                    resistance=None
+                    machine= input('Are you using any machine? (True/False) ')
+                    material= input('What material are you using ? ')
+                    reps= input('Introduce number of reps: ')
+                    sets=input('Introduce number of sets: ')
+                    weight=input('Introduce weight that you are using (kg) : ')
+                cursor3.callproc('add_exercise',args=[exercise_name,workout_id,type,distance,velocity,resistance,machine,material,reps,sets,weight])
+                connection.commit()
+                cursor3.close()
+                successful_operation=True
+            except pymysql.MySQLError as error:
+                print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+                print("Please try again.")
+        successful_operation=False
+        cursor4=connection.cursor()
+        while not successful_operation:
+            try:
+                cursor4.callproc('new_exercise_in_workout', args=[exercise_name,workout_id])
+                connection.commit()
+                cursor4.close()
+                successful_operation=True
+            except pymysql.MySQLError as error:
+                print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+                print("Please try again.")
+        print('Exercise added to workout!')
+    else:
+        print('Option not valid. Operation aborted')
+            
+
+#Function to add workout
+def add_workout(connection,user_id):
+    successful_operation=False
+    cursor1=connection.cursor()
+    while not successful_operation:
+        try:
+            duration=input('Introduce duration of workout (min): ')
+            calories_burned=input('Introduce number of calories burned with this workout: ')
+            cursor1.callproc('add_workout', args=[user_id,duration,calories_burned])
+            connection.commit()
+            cursor1.close()
+            successful_operation=True
+        except pymysql.MySQLError as error:
+            print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+            print("Please try again.")
+    while True:
+        add_exercise=input('Do you want to add an exercise to this workout? (Yes/No) ')
+        if add_exercise=='Yes':
+            cursor2=connection.cursor()
+            sql_stam='select workout_id from workout order by workout_id desc limit 1'
+            cursor2.execute(sql_stam)
+            workout_id=cursor2.fetchone()
+            cursor2.close()
+            add_exercise_to_workout(connection,workout_id)
+        elif add_exercise=='No':
+            print('Workout created successfully!')
+            break
+        else:
+            print('Answer not valid')
+
+
+#Function to edit workout
+def edit_workout(connection):
+    successful_operation=False
+    cursor1=connection.cursor()
+    while not successful_operation:
+        try:
+            cursor2=connection.cursor()
+            sql_stam="select workout_id from workout order by workout_id asc"
+            cursor2.execute(sql_stam)
+            possible_workout_ids=cursor2.fetchall()
+            print('Workout ids: ')
+            for w in possible_workout_ids:
+                print(w)
+            cursor2.close()
+            workout_id=input('Introduce id of workout that you want to modify: ')
+            duration=input('Introduce new duration of workout (min): ')
+            calories_burned=input('Introduce new number of calories burned with this workout: ')
+            cursor1.callproc('add_workout', args=[workout_id,duration,calories_burned])
+            connection.commit()
+            cursor1.close()
+            successful_operation=True
+        except pymysql.MySQLError as error:
+            print(f"Error: {error.args[1]} (SQLSTATE: {error.args[0]})")
+            print("Please try again.")
+    while True:
+        add_exercise=input('Do you want to add an exercise to this workout? (Yes/No) ')
+        if add_exercise=='Yes':
+            add_exercise_to_workout(connection,workout_id)
+        elif add_exercise=='No':
+            print('Workout updated successfully!')
+            break
+        else:
+            print('Answer not valid')
+
+
+
+#Connection with database:
+while True:
+    username=input('Introduce MySQL username: ')
+    password=input('Introduce MySQL password: ')
+    connection = pymysql.connect(
+        host='localhost',  
+        user=username,
+        password=password,
+        database='health_and_fitness_databaseSchema',
+        cursorclass=pymysql.cursors.DictCursor  
+    )
+    if connection:
+        print('Successful connection')
+        break
+    else:
+        print('incorrect credentials')
+
+#Login
+while True:
+    print('Are you registered?')
+    print('1. Yes , login')
+    print('2. No , create an account')
+    choice=input('Select 1 or 2 ')
+    if choice=='1': 
+        valid_id=False
+        while not valid_id:
+            sql_stam= 'SELECT check_valid_id(%d)'
+            user_id=input('Introduce user id : ')
+            cursor=connection.cursor()
+            cursor.execute(sql_stam,user_id)
+            valid_id= cursor.fetchone()
+            cursor.close()
+        break
+    elif choice=='2':
+        user_id=create_account(connection)
+        print('Account created. Your user id is ',user_id)
+    else :
+        print('No valid option')
+
+#Menu
+while True:
+    print('1. Add new goal')
+    print('2. Track health test')
+    print('3. Track meal')
+    print('4. Add workout')
+    print('5. Edit workout')
+    choice=input('Choose one option: ')
+    if choice=='1':
+        add_new_goal(connection,user_id)
+    elif choice=='2':
+        track_health_test(connection,user_id)
+    elif choice=='3' :
+        track_meal(connection,user_id)
+    elif choice== '4' :
+        add_workout(connection,user_id)
+    elif choice== '5' :
+        edit_workout(connection,user_id)
+    else:
+        print('Option not valid')
+    follow_execution=input('Do you want to close session? (Yes/No) ')
+    while follow_execution!='Yes' or follow_execution!='No':
+        print('Answer not valid')
+        follow_execution=input('Do you want to close session? (Yes/No) ')
+    if follow_execution== 'No':
+        connection.close()
+        break
+    
---
+++ health_and_fitness_databaseSchema.sql
@@ -0,0 +1,271 @@
+CREATE DATABASE health_and_fitness;
+USE health_and_fitness;
+-- User table
+CREATE TABLE user (
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+mail VARCHAR(64) NOT NULL,
+first_name VARCHAR(32),
+last_name VARCHAR(32),
+age INT ,
+gender ENUM('female','male','other'),
+height DECIMAL(5,2),
+weight DECIMAL(5,2));
+
+-- Health Test table
+CREATE TABLE health_test (
+test_id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+body_fat DECIMAL(5,2) NOT NULL,
+heart_rate INT NOT NULL,
+blood_pressure INT NOT NULL,
+date DATE NOT NULL,
+FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE);
+
+-- Workout table
+CREATE TABLE workout (
+workout_id INT AUTO_INCREMENT PRIMARY KEY,
+duration INT, -- duration is measured in minutes
+calories_burned INT);
+
+-- Relationship table between workout and user
+CREATE TABLE user_practices_workout(
+user_id INT,
+workout_id INT,
+PRIMARY KEY (user_id,workout_id),
+FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (workout_id) REFERENCES workout(workout_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Exercise {MANDATORY,OR} table
+CREATE TABLE exercise(
+exercise_name VARCHAR(32) PRIMARY KEY,
+type_exercise ENUM('cardio','strength') NOT NULL,
+distance DECIMAL(4,2) ,
+velocity DECIMAL(3,1) ,
+resistance INT,
+machine BOOL,
+material VARCHAR(32),
+reps INT,
+sets INT,
+weight DECIMAL(5,2),
+CONSTRAINT type_contraint CHECK (
+	(type_exercise='cardio' AND distance != NULL AND velocity != NULL AND resistance != NULL)
+    OR
+    (type_exercise='strength' AND machine != NULL AND reps != NULL AND sets!= NULL AND weight!= NULL)
+)
+);
+
+-- Relationship table between workout and exercises
+CREATE TABLE workout_exercise(
+workout_id INT,
+exercise_name VARCHAR(32),
+PRIMARY KEY (workout_id,exercise_name),
+FOREIGN KEY (workout_id) REFERENCES workout(workout_id) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (exercise_name) REFERENCES exercise(exercise_name) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Goal table
+CREATE TABLE goal(
+goal_id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+type VARCHAR(32) NOT NULL,
+target_value INT,
+start_date DATE NOT NULL,
+target_date DATE,
+status ENUM('in progress','finished','not started'),
+FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE);
+
+-- Meal table
+CREATE TABLE meal(
+meal_id INT AUTO_INCREMENT PRIMARY KEY,
+goal_id INT NOT NULL,
+user_id INT NOT NULL,
+meal_date DATE NOT NULL,
+meal_type VARCHAR(32) ,
+calories INT NOT NULL,
+protein DECIMAL(4,2),
+carbs DECIMAL(4,2),
+fat DECIMAL(4,2),
+FOREIGN KEY (goal_id) REFERENCES goal(goal_id) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE);
+
+-- PROCEDURE to create a new user
+DELIMITER //
+CREATE PROCEDURE create_user (IN mail_p VARCHAR(64) ,
+IN first_name_p VARCHAR(32),
+IN last_name_p VARCHAR(32),
+IN age_p INT ,
+IN gender_p ENUM('female','male','other'),
+IN height_p DECIMAL(5,2),
+IN weight_p DECIMAL(5,2))
+BEGIN
+	DECLARE exist_mail INT ;
+    SELECT COUNT(*) INTO exist_mail FROM user WHERE mail=mail_p ;
+    IF exist_mail !=0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'mail already associated to an user';
+	ELSEIF mail_p IS NULL THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'introduce a mail';
+	ELSE 
+        INSERT INTO user( mail, first_name , last_name , age, gender, height , weight)
+			VALUES ( mail_p , first_name_p , last_name_p , age_p , gender_p , height_p , weight_p) ;
+	END IF ;
+END //
+DELIMITER ;
+
+-- FUNCTION to see if an user id exists
+DELIMITER //
+CREATE FUNCTION check_valid_id (user_id_p INT) RETURNS BOOL DETERMINISTIC
+BEGIN
+	DECLARE exist_id INT ;
+    SELECT COUNT(*) INTO exist_id FROM user WHERE user_id=user_id_p;
+    IF exist_id=0 THEN
+		RETURN False;
+	ELSE
+		RETURN True;
+	END IF ;
+END//
+DELIMITER ;
+
+
+-- PROCEDURE to add goal
+DELIMITER //
+CREATE PROCEDURE add_goal( IN user_id_p INT,
+IN type_p VARCHAR(32) ,
+IN target_value_p INT,
+IN start_date_p DATE,
+IN target_date_p DATE,
+IN status_p ENUM('in progress','finished','not started'))
+BEGIN
+    INSERT INTO goal (user_id,type, target_value, start_date , target_date, status) 
+    VALUES (user_id_p , type_p , target_value_p , start_date_p , target_date_p , status_p);
+END //
+DELIMITER ;
+
+
+-- PROCEDURE to track health test
+DELIMITER //
+CREATE PROCEDURE track_health_test ( IN user_id_p INT ,
+IN body_fat_p DECIMAL(5,2) ,
+IN heart_rate_p INT ,
+IN blood_pressure_p INT ,
+IN date DATE )
+BEGIN
+    INSERT INTO health_test (user_id , body_fat ,heart_rate ,blood_pressure , date )
+		VALUES (user_id_p, body_fat_p , heart_rate_p , blood_pressure_p , date_p) ; 
+END //
+DELIMITER ;
+
+-- PROCEDURE to track meal
+DELIMITER //
+CREATE PROCEDURE track_meal ( IN goal_id_p INT ,
+IN user_id_p INT,
+IN meal_date_p DATE,
+IN meal_type_p VARCHAR(32) ,
+IN calories_p INT ,
+IN protein_p DECIMAL(4,2),
+IN carbs_p DECIMAL(4,2),
+IN fat_p DECIMAL(4,2))
+BEGIN
+	DECLARE exist_goal INT;
+    SELECT COUNT(*) INTO exist_goal FROM goal WHERE goal_id=goal_id_p;
+    IF exist_goal = 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT= 'unvalid goal';
+	ELSE 
+        INSERT INTO meal (goal_id,user_id, meal_date, meal_type, calories, protein, carbs, fat)
+			VALUES (goal_id_p ,user_id_p , meal_date_p , meal_type_p , calories_p, protein_p, carbs_p, fat_p);
+	END IF;
+    
+END //
+DELIMITER ;
+
+-- PROCEDURE to add workout
+DELIMITER //
+CREATE PROCEDURE add_workout (IN user_id_p INT ,
+IN duration_p INT, 
+IN calories_burned_p INT)
+BEGIN
+	-- we have to add new workout to workout table
+    INSERT INTO workout(duration, calories_burned)
+		VALUES (duration_p , calories_burned_p) ; 
+    -- we have to add new workout-user relationship into user_practices_workout table
+    INSERT INTO user_practices_workout ( user_id , workout_id ) 
+		VALUES (user_id_p ,last_workout +1);
+END//
+DELIMITER ;
+
+-- PROCEDURE to edit workout
+DELIMITER //
+CREATE PROCEDURE edit_workout ( IN workout_id_p INT,
+IN new_duration INT,
+IN new_calories_burned INT )
+BEGIN
+	DECLARE exist_workout INT ;
+    SELECT COUNT(*) INTO exist_workout FROM workout WHERE workout_id = workout_id_p;
+    IF exist_workout != 0 THEN 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'workout does not exist';
+	ELSE 
+		UPDATE workout SET duration = new_duration , calories_burned = new_calories_burned WHERE workout_id=workout_id_p ; 
+	END IF ;
+END //
+DELIMITER ;
+
+-- trigger to no update workout if new values for duration and calories burned are the same as the old ones
+DELIMITER //
+CREATE TRIGGER before_updte_workout BEFORE UPDATE ON workout FOR EACH ROW
+BEGIN
+	IF NEW.duration = OLD.duration AND NEW.calories_burned = OLD.calories_burned THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No changes in workout. Update not performed' ;
+	END IF ;
+END //
+DELIMITER ;
+
+-- PROCEDURE to add exercise 
+DELIMITER //
+CREATE PROCEDURE add_exercise ( IN exercise_name_p VARCHAR(32) ,
+IN workout_p INT ,
+IN type_exercise_p ENUM('cardio','strength') ,
+IN distance_p DECIMAL(4,2) ,
+IN velocity_p DECIMAL(3,1) ,
+IN resistance_p INT,
+IN machine_p BOOL,
+IN material_p VARCHAR(32),
+IN reps_p INT,
+IN sets_p INT,
+IN weight_p DECIMAL(5,2) )
+BEGIN
+	DECLARE exist_exercise INT;
+	-- let's see first if exercise it is already created
+    SELECT COUNT(*) INTO exist_exercise FROM exercise WHERE exercise_name=exercise_name_p ;
+    IF exist_exercise = 0 THEN
+		INSERT INTO exercise(exercise_name,type_exercise,distance,velocity, resistance, machine , material,reps ,sets , weight)
+			VALUES (exercise_name_p,type_exercise_p,distance_p,velocity_p, resistance_p, machine_p , material_p,reps_p ,sets_p , weight_p) ;
+	END IF ;
+END// 
+DELIMITER ;
+
+-- PROCEDURE to add exercise to a workout
+DELIMITER //
+CREATE PROCEDURE new_exercise_in_workout(IN exercise_name_p VARCHAR(32),IN workout_id_p INT)
+BEGIN
+	DECLARE exist_exercise INT;
+    SELECT COUNT(*) INTO exist_exercise FROM exercise WHERE exercise_name=exercise_name_p ;
+    IF exist_exercise = 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='Exercise does not exist';
+	ELSE 
+		INSERT INTO workout_exercise (workout_id,exercise_name) 
+			VALUES(workout_id_p,exercise_name_p);
+	END IF;
+END//
+DELIMITER ;
+
+-- create trigger to see if exercise is on workout already
+DELIMITER //
+CREATE TRIGGER before_insert_exercise BEFORE INSERT ON workout_exercise FOR EACH ROW
+BEGIN
+	DECLARE exist_exercise_in_wk INT;
+    SELECT COUNT(*) INTO exist_exercise_in_wk FROM workout_exercise WHERE workout_id= NEW.workout_id AND exercise_name=NEW.exercise_name ;
+    IF exist_exercise_in_wk != 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'exercise already in workout. Insertion not performed';
+	END IF ;
+END //
+DELIMITER ;
